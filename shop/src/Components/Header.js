a2496e9752183457c4de99a92fad7d85f038665b
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-04-17 21:10:22 +0200

    1

--- shop/src/Components/Header.js
+++ shop/src/Components/Header.js
@@ -5,13 +5,13 @@ const Header = () => {
 
     return(
         <h1 className='navbar'>
-            <Link to={`/`} className='buttonLink' >
+            <Link to={`/`} className='buttonLink1' >
                 <button className='button1'>Home</button>
             </Link>
-            <Link to={`/`} className='buttonLink' >
+            <Link to={`/`} className='buttonLink2' >
                 <button className='button2'>Login</button>
             </Link>
-            <Link to={`/`} className='buttonLink' >
+            <Link to={`/`} className='buttonLink3' >
                 <button className='button3'>Checkout</button>
             </Link>
         </h1>
