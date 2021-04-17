5a0edbf94ca83e2b9a9c9cd02bbe5c327634fafa
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-04-17 19:19:45 +0200

    1

--- shop/src/Components/Header.js
+++ shop/src/Components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const Header = () => {
 
     return(
         <h1 className='navbar'>
-            <button className='button1'>Home</button>
-            <button className='button2'>Login</button>
-            <button className='button3'>Checkout</button>
+            <Link to={`/`} className='buttonLink' >
+                <button className='button1'>Home</button>
+            </Link>
+            <Link to={`/`} className='buttonLink' >
+                <button className='button2'>Login</button>
+            </Link>
+            <Link to={`/`} className='buttonLink' >
+                <button className='button3'>Checkout</button>
+            </Link>
         </h1>
     )
 
