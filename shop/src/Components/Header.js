d04953f0b9be7f5edf8eb7c1092357eae24a88da
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-04-22 12:48:38 +0200

    1

--- shop/src/Components/Header.js
+++ shop/src/Components/Header.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { ItemContext } from './Context';
+import './Header.css'
 
 const Header = () => {
     const {user, setUser, loggedIn, setLoggedIn} = useContext(ItemContext);
+    const [listOn, setListOn] = useState(false);
+    const lista = () => {
+        setListOn(true);
+    }
+    const listaOff = () => {
+        setListOn(false);
+    }
 
     return(
         <h1 className='navbar'>
@@ -14,9 +22,15 @@ const Header = () => {
             <Link to={`/login`} className='buttonLink2' >
                 <button className='button2'>Login</button>
             </Link>
+            : listOn === false ?
+            <Link to={`/profile`} className='profilLink' onMouseEnter={lista} onClick={listaOff} >
+                <button className='profilBotun'>{user.name}</button> 
+            </Link>
             :
-            <Link to={`/profile`} className='buttonLink2' >
-                <button className='button2'>{user.name}</button>
+            <Link to={`/profile`} className='profilLink' onClick={listaOff}>
+                <button className='profilBotun'>{user.name}</button>
+                <button onMouseLeave={listaOff}>Create listing</button>
+                <button onMouseLeave={listaOff}>My listings</button>
             </Link>
             }      
             <Link to={`/checkout`} className='buttonLink3' >
