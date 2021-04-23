e4c91b0944123c1e986067e890be2cabe4d53d9a
bokekez <69863141+bokekez@users.noreply.github.com>
2021-04-23 19:58:39 +0200

    Update Server.js

--- shopapi/Server.js
+++ shopapi/Server.js
@@ -9,22 +9,27 @@ const app = express();
 app.use(cors())
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        // connectionString: process.env.DATABASE_URL,
-        // ssl: true,
+     client: 'pg',
+     connection: {
+    //     // connectionString: process.env.DATABASE_URL,
+    //     // ssl: true,
       // host : 'postgresql-amorphous-40499',
-      // user : 'postgres',
-      // password : 'test',
-      // database : 'smartbrain'
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'test',
+      database : 'shop'
     }
-  });
+});
 
   app.get('/', (req, res) =>{
-      db.select('id', 'item', 'price', 'sales').from('katalog')
+      db.select('id', 'item', 'price', 'sales').from('items')
       .then(data =>{
           res.json(data)
       })
   })
+
+console.log('10 4 dinosaur')
+app.listen(3000);
